05b5973cddc8148ca0c6d74d90452883c2806b3f
echo-mei <739181006@qq.com>
2017-11-22 15:56:45 +0800

    commit4
    demo里面index报错问题

--- demo/self_defined_search.js
+++ demo/self_defined_search.js
@@ -116,49 +116,12 @@ $(function () {
     // 姓名查询可输入多值
    $("#sf-name").multiInput();
 
-    /*************多选下拉树(使用ztree树结构)*****************/
-    // zTree 的参数配置，深入使用请参考 API 文档（setting 配置详解）
-    var setting = {
-            check: {
-                enable: true,
-                chkboxType: { "Y": "", "N": "" }
-            },
-            data: {
-                simpleData: {
-                    enable: true
-                }
-            },
-            view:{
-                showIcon:false
-            }
-        };
-    // zTree 的数据属性，深入使用请参考 API 文档（zTreeNode 节点数据详解）
-    var zNodes =[
-        { id:1, pId:0, name:"随意勾选 1", open:true},
-        { id:11, pId:1, name:"随意勾选 1-1", open:true},
-        { id:111, pId:11, name:"随意勾选 1-1-1"},
-        { id:112, pId:11, name:"随意勾选 1-1-2"},
-        { id:12, pId:1, name:"随意勾选 1-2", open:true},
-        { id:121, pId:12, name:"随意勾选 1-2-1"},
-        { id:122, pId:12, name:"随意勾选 1-2-2"},
-        { id:2, pId:0, name:"随意勾选 2",  open:true},
-        { id:21, pId:2, name:"随意勾选 2-1"},
-        { id:22, pId:2, name:"随意勾选 2-2", open:true},
-        { id:221, pId:22, name:"随意勾选 2-2-1"},
-        { id:222, pId:22, name:"随意勾选 2-2-2"},
-        { id:23, pId:2, name:"随意勾选 2-13"}
-    ];
-
+    //多选下拉树(使用ztree树结构)
     $('#ksName').dropdownTree({
-        zNodes: zNodes,
-        treeSettings:setting,
-        idSaveEle:$("#ksId")
+        zNodesUrl: "../dropdown/dropdownTree.json",
+        idSaveEle:$("#dropdownTreeId")
     });
-    $('#ks2Name').dropdownTree({
-        zNodes: zNodes,
-        treeSettings:setting,
-        idSaveEle:$("#ks2Id")
-    });
+
 
     // 下拉多选
     $("#xlName").dropdown({
